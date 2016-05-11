0d8d796b99eb6e08c6e893fb950452ce39bfcc6d
prasenjitj <Prasenjit.jana@gmail.com>
2016-05-11 15:50:51 +0530

    Add files via upload

--- cl edits/soccer_dedup_v2.py
+++ cl edits/soccer_dedup_v2.py
@@ -1,4 +1,4 @@
-
+import csv
 """converts raw data to thali format for mlb leagues."""
 print 'soccer dedup script invoked'
 
@@ -8,6 +8,9 @@ with open('soccer_thali.csv','rb') as csvinput:
     fieldnames = ['task_type','task_name','url','external_id','tag','year','description'] 
     writer = csv.DictWriter(csvoutput, fieldnames=fieldnames)
     writer.writeheader()
-    
+    entries = set()
     for row in reader:
-      row['external_id'] =
+      key = (row['external_id'])
+      if key not in entries:
+        writer.writerow(row)
+        entries.add(key) 
